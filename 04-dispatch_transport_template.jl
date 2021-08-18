c33c086f20fa2f891fa85dfe438ae6767b4bed0b
Lukas174 <77328471+Lukas174@users.noreply.github.com>
2021-08-18 19:07:08 +0200

    Add files via upload

--- 04-dispatch_transport_template.jl
+++ 04-dispatch_transport_template.jl
@@ -1,21 +1,23 @@
-using DataFrames
-using Plots
 using JuMP
-using Clp
+# using Clp
+# import Pkg
+# Pkg.add("Gurobi")
+# Pkg.build("Gurobi")
+using Gurobi
 using Plots, StatsPlots
 using DataFrames, CSV 
 
-
-# Preprocessing
+using Statistics
+#  Preprocessing
 ### data load ###
-datapath = "data/Dummy_Data"
+datapath = joinpath(@__DIR__, "data")
 # datapath = "data"
 
 timeseries_path = joinpath(datapath, "time_series")
 static_path = joinpath(datapath, "static")
 
 plants =  CSV.read(joinpath(static_path,"plants.csv"), DataFrame)
-ntc =  CSV.read(joinpath(static_path,"ntc.csv"), DataFrame)
+ntc_data =  CSV.read(joinpath(static_path,"ntc.csv"), DataFrame)
 
 
 timeseries = Dict(splitext(files)[1] => CSV.read(joinpath(timeseries_path, files), DataFrame)
@@ -26,12 +28,15 @@ timeseries = Dict(splitext(files)[1] => CSV.read(joinpath(timeseries_path, files
 
 ### sets ###
 T = 1:size(timeseries["demand_2015"], 1) |> collect
+# T = 1:4000 |> collect
 TECH = unique(plants[:,:id] |> Vector)
 DISP = unique(plants[plants[:,:disp] .== 1, :id])
-NONDISP =  unique(plants[plants[:,:disp] .== 0, :id])
+NONDISP = unique(plants[plants[:,:disp] .== 0, :id])
 S = plants[plants[:,:storage_capacity] .!= 0, :id]
-Z = unique(ntc[:,:from_country])
-# P = zones[:,:id] |> Vector
+Z = unique(ntc_data[:,:from_country])
+CU = plants[plants[:,:tech] .== "CU", :id] |> Vector
+CU_NEG = plants[plants[:,:tech] .== "CU_neg", :id] |> Vector
+
 ### parameters ###
 
 ### utility functions and mappings ###
@@ -44,6 +49,8 @@ function dictzip(df::DataFrame, x::Pair)
     return zip(dictkeys, dictvalues) |> collect |> Dict
 end
 
+coldict(df::DataFrame) = Dict(string(name) => Vector(vec) for (name,vec) in pairs(eachcol(df)))
+
 ### Static Parameters ###
 mc = dictzip(plants, :id => :mc_el)
 g_max = dictzip(plants, :id => :g_max)
@@ -51,17 +58,26 @@ d_max = dictzip(plants, :id => :d_max)
 eta = dictzip(plants, :id => :eta)
 storage_capacity = dictzip(plants, :id => :storage_capacity)
 
-# map_id2country = dictzip(plants, :id => :country)
 map_country2id = Dict()
 for z in Z
     map_country2id[z] = (plants[plants.country .== z, "id"])
 end
-map_country2id = Dict()
-for z in Z
-    map_country2id[z] = (plants[plants.country .== z, "id"])
+
+map_id2country = Dict()
+for plant_id in plants[:,:id] 
+    map_id2country[plant_id] = unique((plants[plants.id .== plant_id, "country"]))[1]
 end
-map_id2country = dictzip(plants,  :id => :country)
-map_id2tech = dictzip(plants,  :id => :tech)
+
+map_id2tech  = Dict()
+for plant_id in plants[:,:id] 
+    map_id2tech[plant_id] = unique((plants[plants.id .== plant_id, "tech"]))[1]
+end
+# Hyrdo Inflows
+psp_inflow = coldict(timeseries["hydro_psp_inflow_countries"])
+PSP_list = plants[plants[:,:tech] .== "hydro_psp", :id]
+res_inflow = coldict(timeseries["hydro_res_inflow_countries"])
+RES_list = plants[plants[:,:tech] .== "hydro_res", :id]
+
 ### time series ###
 coldict(df::DataFrame) = Dict(string(name) => Vector(vec) for (name,vec) in pairs(eachcol(df)))
 
@@ -71,6 +87,7 @@ elec_demand = coldict(timeseries["demand_2015"])
 feed_in_wind_off = Dict()
 feed_in_wind_on = Dict()
 feed_in_pv = Dict()
+feed_in_ror = Dict()
 feed_in = Dict()
 
 for z in Z
@@ -85,7 +102,12 @@ for z in Z
     if ("solar" in  plants[(plants.country .== z),"tech"]) & (z in names(timeseries["pv_2015"]))
         feed_in_pv[z] = timeseries["pv_2015"][!,z] .* (plants[(plants.country .== z) .& (plants.tech .== "solar"), "g_max"])
     end
-    feed_in[z] = [0 for i=1:size(T)[1]]
+    if ("ror" in  plants[(plants.country .== z),"tech"])
+        feed_in_ror[z] = [0.65 for i=1:8760] .* (plants[(plants.country .== z) .& (plants.tech .== "ror"), "g_max"])
+        # feed_in_ror[z] = [0.65 for i=1:size(T)[1]] .* (plants[(plants.country .== z) .& (plants.tech .== "ror"), "g_max"])
+    end
+    feed_in[z] = [0 for i=1:8760]
+    # feed_in[z] = [0 for i=1:size(T)[1]]
 end
 
 # Summing the different feedins 
@@ -93,6 +115,8 @@ for z in Z
     if haskey(feed_in_wind_off, z) feed_in[z] = feed_in[z] .+ feed_in_wind_off[z] end
     if haskey(feed_in_wind_on, z) feed_in[z] = feed_in[z] .+ feed_in_wind_on[z] end
     if haskey(feed_in_pv, z) feed_in[z] = feed_in[z] .+ feed_in_pv[z] end
+    if haskey(feed_in_ror, z) feed_in[z] = feed_in[z] .+ feed_in_ror[z] end
+
 end
 
 feed_in_by_z_nondisp= Dict()
@@ -100,42 +124,48 @@ feed_in_by_z_nondisp= Dict()
 for x in keys(feed_in_wind_off)
     feed_in_by_z_nondisp[x,"wind offshore"] = feed_in_wind_off[x]
 end
+
 for x in keys(feed_in_wind_on)
     feed_in_by_z_nondisp[x,"wind onshore"] = feed_in_wind_on[x]
 end
+
 for x in keys(feed_in_pv)
     feed_in_by_z_nondisp[x,"solar"] = feed_in_pv[x]
 end
-
-
 
 ### sucessor: give the next index of an array, return 1 for the last element###
 successor(arr, x) = x == length(arr) ? 1 : x + 1
 
 ### ntc ###
-ntc = dictzip(ntc, [:from_country, :to_country] => :ntc)
-for (k,v) in ntc ntc[reverse(k)] = v end
+ntc = dictzip(ntc_data, [:from_country, :to_country] => :ntc)
 
 # actual model creation
 ###############################################################################
-m = Model(Clp.Optimizer)
+m = Model(Gurobi.Optimizer)
+set_optimizer_attribute(m, "TimeLimit", 600)
+
 
 @variables m begin
     g_max[disp] >= G[disp=DISP, T] >= 0
-    CU[Z,T] >= 0
     d_max[s] >= D_stor[s=S,T] >= 0
     storage_capacity[s] >= L_stor[s=S,T] >= 0
     EX[z=Z,zz=Z,T] >= 0
+    CU[Z,T] >= 0
+    BALANCE_P[Z,T] >= 0
+
 end
 
 @objective(m, Min,
-    sum(mc[disp] * G[disp,t] for disp in DISP, t in T));
+    sum(mc[disp] * G[disp,t] for disp in DISP, t in T)
+    + sum((CU[z,t] + BALANCE_P[z,t])*1000 for z in Z, t in T)
+    );
 
 @constraint(m, ElectricityBalance[z=Z, t=T],
     sum(G[disp,t] for disp in intersect(map_country2id[z],DISP))
     + feed_in[z][t]
     - sum(D_stor[s,t] for s in intersect(map_country2id[z],S))
     - CU[z,t]
+    + BALANCE_P[z,t]
     + sum(EX[zz,z,t] - EX[z,zz,t] for zz in Z)
     ==
     elec_demand[z][t])
@@ -150,13 +180,18 @@ end
     ==
     L_stor[s, t]
     + sqrt(eta[s])*D_stor[s,t]
-    - (1/sqrt(eta[s])) * G[s,t])
+    - (1/sqrt(eta[s])) * G[s,t]
+    + (s in PSP_list ? psp_inflow[map_id2country[s]][t] : 0)
+    + (s in RES_list  ? res_inflow[map_id2country[s]][t] : 0)
+    );
 
 optimize!(m)
+
 
 # post processing
 ########################################################
 generic_names(len::Int) = [Symbol("x$i") for i in 1:len]
+
 function DataFrame(x::JuMP.Containers.DenseAxisArray,
     names::AbstractVector{Symbol}=generic_names(length(x.axes));
     kwargs...)
@@ -179,15 +214,13 @@ insertcols!(result_G, 2, :zone => [map_id2country[id] for id in result_G[!,:id]]
 insertcols!(result_G, 3, :technology => [map_id2tech[id] for id in result_G[!,:id]])
 
 # Debugging
-result_G[result_G[:,:zone].=="CH",:]
-plants[plants[:,:disp] .== 0, :id]
+
 
 
 # Creating new result DF to collect exchanges between zones
 result_EX_data = DataFrame(EX, [:from_Z, :to_Z, :hour] )
 # Filtering out entries from and to same zone (as thos will by definition allways be 0)
 result_EX_data = result_EX_data[result_EX_data.from_Z .!= result_EX_data.to_Z, :]
-
 # dividing the exchange to only import data
 res_imp = copy(result_EX_data) # fetching a copy to not alter the orginal DF
 insertcols!(res_imp, 3, :technology => "import") #adding column with marking 'import'
@@ -204,7 +237,163 @@ rename!(res_exp, :from_Z => :zone) # renaming the column to uniform 'zone' label
 # recombining the DFs import and export
 result_EX = vcat(res_imp, res_exp)
 
+##########################################################################
 
+# TABLE 1
+
+id_h_value = dictzip(result_G_TEST, [:id,:hour] => :value)
+
+tech_country = dictzip(plants, :id => :country)
+
+# Summer Timing (Astronomical Season): 21. June[171*24 = 4104] - 23. September[265*24 = 6360]
+# Winter Timing (Astronomical Season): 1. Januar - 20. März[78*24 = 1872] ; 22. Dezember - 31. Dezember[10*24 = 240]
+T_s = 4104:6360
+T_w1 = 0:1872
+T_w2 = 8520:8760
+T_w = vcat(T_w1,T_w2)
+
+sum_tech = DataFrame(
+    (technology = tech,
+    country = tech_country[tech],
+    value_annual = sum( (tech,t) in keys(id_h_value) ? id_h_value[tech,t] : 0 for t in T),
+    value_summer = sum( (tech,t) in keys(id_h_value) ? id_h_value[tech,t] : 0 for t in T_s),
+    value_winter = sum( (tech,t) in keys(id_h_value) ? id_h_value[tech,t] : 0 for t in T_w)
+    )
+    for tech in TECH
+)
+
+avg_annual_z = dictzip(sum_tech, [:technology, :country] => :value_annual)
+avg_summer_z = dictzip(sum_tech, [:technology, :country] => :value_summer)
+avg_winter_z = dictzip(sum_tech, [:technology, :country] => :value_winter)
+
+# Durchschnittlichen Marginal cost * prodcution
+# Durchschnittlichen Marginal cost sum(mc_cost*production_technology for Z)/ (production in country Z)
+table1 = DataFrame(
+    (zone = z,
+    avg_price_annual= sum((id,z) in keys(avg_annual_z) ? avg_annual_z[id,z] : 0 for id in sum_tech.technology)/8760,
+    avg_price_summer= sum((id,z) in keys(avg_summer_z) ? avg_summer_z[id,z] : 0 for id in sum_tech.technology)/8760,
+    avg_price_winter= sum((id,z) in keys(avg_winter_z) ? avg_winter_z[id,z] : 0 for id in sum_tech.technology)/8760,
+    )
+    for z in Z
+)
+
+
+result_G_TEST = copy(result_G)
+result_G_TEST = select!(result_G_TEST, Not(:technology))
+# result_G_TEST = select!(result_G_TEST, Not(:id))
+
+zone_value = dictzip(result_G_TEST, [:zone, :hour, :id] => :value)
+
+test = DataFrame()
+for t in T
+    test[!,z] = ss
+end
+
+variance = convert(Array, result_G_TEST.value)
+
+mc_annual = dictzip(result_G_TEST, [:zone,:hour] => :value)
+
+
+# TEST
+# plants_mc = dictzip(plants, [:id, :country] => :mc_el)
+
+# result_G_summer = copy(result_G)
+# result_G_summer = filter!(row -> 6360>row.hour>4103 , result_G_TEST)
+# mc_annual_s = dictzip(result_G_TEST, [:id, :zone,:hour] => :value)
+
+# T_s = 4104:6360
+
+# T_w1 = 0:1872
+# T_w2 = 8520:8760
+# T_w = vcat(T_w1,T_w2)
+
+
+# avg_price_annual_z = DataFrame()
+# avg_price_annual_summer_z = DataFrame()
+# avg_price_annual_winter_z = DataFrame()
+# # ANNUAL
+# for z in Z
+#     avg_price_annual_z[!,z] = sum((id,z) in keys(plants_mc) && (id,z,t) in keys(mc_annual) ? plants_mc[id, z]*mc_annual[id,z,t] : 0 for id in result_G.id, t in T)/8760
+# end
+
+# # SUMMER
+# for z in Z
+#     avg_price_annual_summer_z[!,z] = 
+#         sum((id,z) in keys(plants_mc) && (id,z,t) in keys(mc_annual) ? plants_mc[id, z]*mc_annual[id,z,t] : 0 for id in result_G.id, t in T_s)/8760
+# end
+
+# # WINTER
+# for z in Z
+#     avg_price_annual_winter_z[!,z] = 
+#         sum((id,z) in keys(plants_mc) && (id,z,t) in keys(mc_annual) ? plants_mc[id, z]*mc_annual[id,z,t] : 0 for id in result_G.id, t in T_w)/8760
+# end
+
+# table1 = DataFrame(
+#     (zone = z,
+#     avg_price_annual= sum((id,z) in keys(plants_mc) && (id,z,t) in keys(mc_annual) ? plants_mc[id, z]*mc_annual[id,z,t] : 0 for id in TECH, t in T)/8760,
+#     avg_price_summer= sum((id,z) in keys(plants_mc) && (id,z,t) in keys(mc_annual) ? plants_mc[id, z]*mc_annual[id,z,t] : 0 for id in TECH, t in T_s)/8760,
+#     avg_price_winter= sum((id,z) in keys(plants_mc) && (id,z,t) in keys(mc_annual) ? plants_mc[id, z]*mc_annual[id,z,t] : 0 for id in TECH, t in T_w)/8760,
+#     )
+#     for z in Z
+# )
+
+
+# TABLE 2
+ntc_time = dictzip(result_EX_data, [:from_Z, :to_Z, :hour] => :value)
+
+table2 = DataFrame(
+    (from_zone = z,
+    to_zone = zz,
+    ntc_max = ntc[z,zz],
+    avg_sum_year = sum(ntc_time[z,zz,t] for t in T)/8760,
+    utilisation_percent =  ntc[z,zz] != 0 ? (sum(ntc_time[z,zz,t] for t in T)/8760)/ntc[z,zz] : 0,
+    utilisation_equal100 = ntc[z,zz] != 0 ? sum(ntc[z,zz] == ntc_time[z,zz,t] ? 1 : 0 for t in T)/8760 : 0,
+    utilisation_bigger90 = ntc[z,zz] != 0 ? sum(ntc[z,zz]*0.9 <= ntc_time[z,zz,t] ? 1 : 0 for t in T)/8760 : 0,
+    )
+    for z in Z, zz in Z if z!=zz
+)
+CSV.write("/Users/lukasschirren/desktop/FromTo_ntc_utilisation.csv", table2)
+
+
+
+# TABLE 3
+#TEST
+#export_FR = res_exp[findall(x -> occursin("FR", x), res_exp[!, :zone]), :]
+#sum_FR = sum(export_FR.value)/8760
+#export_FR_ntc_MAX = ntc_data()
+#CSV.write("/Users/lukasschirren/desktop/inflow_AT.csv", export_DE)
+
+# max NTC capacities (import/export)
+fixed_ntc_in = DataFrame()
+fixed_ntc_out = DataFrame()
+for z in Z
+    fixed_ntc_in[!,z] = (ntc_data[findall(x -> occursin(z,x), ntc_data[!,:to_country]),:]).ntc
+    fixed_ntc_out[!,z] = (ntc_data[findall(x -> occursin(z,x), ntc_data[!,:from_country]),:]).ntc
+end
+
+
+# Inflow/Outflow over the whole year
+import_z = DataFrame()
+export_z = DataFrame()
+for z in Z
+    import_z[!,z] = (res_imp[findall(x -> occursin(z, x), res_imp[!, :zone]), :]).value
+    export_z[!,z] = (res_exp[findall(x -> occursin(z, x), res_exp[!, :zone]), :]).value
+end
+
+table3 = DataFrame(
+    (zone=z,
+    max_import_ntc = sum(fixed_ntc_in[!,z]),
+    avg_import = sum(import_z[!,z])/8760,
+    max_export_ntc = sum(fixed_ntc_out[!,z]),
+    avg_export = sum(export_z[!,z])/8760,
+    utilisation_in = (sum(import_z[!,z])/8760)/sum(fixed_ntc_in[!,z]),
+    utilisation_out = -(sum(export_z[!,z])/8760)/sum(fixed_ntc_out[!,z])
+    )
+    for z in Z
+)
+CSV.write("/Users/lukasschirren/desktop/Country_ntc_import_export.csv", table3)
+
+##########################################################################
 
 gen_by_tech = combine(groupby(result_G, [:zone, :technology, :hour]),
     :value => sum => :value)
@@ -224,6 +413,11 @@ gen_by_tech2 = combine(groupby(result_feedin, [:zone, :technology, :hour]),
 result_CU = DataFrame(CU, [:zone, :hour])
 result_CU[!,:technology] .= "curtailment"
 
+############################### BALANCE_P muss noch im weiteren aufgenommen werden ######################################
+result_BALANCE_P = DataFrame(BALANCE_P, [:zone, :hour])
+result_BALANCE_P[!,:technology] .= "Balancing_Power"
+############################### BALANCE_P muss noch im weiteren aufgenommen werden ######################################
+
 result_D = DataFrame(D_stor, [:id, :hour])
 insertcols!(result_D, 2, :zone => [map_id2country[id] for id in result_D[!,:id]])
 insertcols!(result_D, 3, :technology => [map_id2tech[id] for id in result_D[!,:id]])
@@ -238,8 +432,8 @@ result_demand = vcat(demand_by_storage, result_CU, electricity_demand)
 
 #added color for exchange (same color as it is clearly visible that export is negative and import positive)
 colordict = Dict(
-    "pv" => :yellow, "wind" => :lightblue, "pumped_hydro" => :darkblue,
-    "battery" => :lightgrey, "p1" => :brown, "p2" => :grey, "demand" => :darkgrey,
+    "solar" => :yellow, "wind offshore" => :lightblue, "wind onshore" => :lightblue, "hydro_psp" => :darkblue, "hydro_res" => :darkblue, "ror" => :blue, 
+    "ccgt" => :lightgrey, "ccot" => :lightgrey,"generator" => :lightgrey, "steam" => :brown, "geothermal" => :pink, "demand" => :darkgrey,
     "curtailment" => :red, "import" => :purple, "export" => :purple) 
 
 function plot_energybalance(df_gen::DataFrame, df_dem::DataFrame, df_ex::DataFrame, z::AbstractString)
